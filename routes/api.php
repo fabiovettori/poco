32d4c29ce4d10ce46f0c70981ca5aa8b55f72700
Fabio Vettori <fabio.vettori.ing@gmail.com>
2021-03-22 19:20:05 +0100

    Models

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::namespace('Api')->group(function(){
     Route::get('/categories', 'CategoriesController@show');
     Route::get('/products', 'ProductsController@show');
-})
+});
