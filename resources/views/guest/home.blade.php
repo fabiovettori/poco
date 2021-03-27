8f412a9784fea73f87d6ec71c8b40f8917230603
Fabio Vettori <fabio.vettori.ing@gmail.com>
2021-03-27 19:36:25 +0100

    Highlights section

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -4,38 +4,77 @@
 
 @section('content')
     <section class="hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="call-to-action">
-                        <h2 class="text-uppercase">Unlimited <br> medium <span>pizzas</span></h2>
-                        <h3>Medium 2-topping* pizza</h3>
-                        <p>*Additional charge for premium toppings. Minimum of 2 required.</p>
-                        <div>
-                            <a class="text-uppercase" href="#">order now</a>
-                            <span>$12.99</span>
-                            <span>$19.99</span>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="call-to-action">
+                            <h2 class="text-uppercase">Unlimited <br> medium <span>pizzas</span></h2>
+                            <h3>Medium 2-topping* pizza</h3>
+                            <p>*Additional charge for premium toppings. Minimum of 2 required.</p>
+                            <div>
+                                <a class="text-uppercase" href="#">order now</a>
+                                <span>$12.99</span>
+                                <span>$19.99</span>
+                            </div>
+                            <img src=" {{ asset('images/hero_pizza.png') }} " alt="pizza">
                         </div>
-                        <img src=" {{ asset('images/hero_pizza.png') }} " alt="pizza">
+                        <a class="text-uppercase" name="menus" href="#">Menus</a>
                     </div>
-                    <a class="text-uppercase" name="menus" href="#">Menus</a>
                 </div>
             </div>
         </div>
     </section>
 
     <section class="categories">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <ul class="list-unstyled m-0">
-                        @foreach ($categories as $category)
-                            <li>
-                                <img src=" {{ $category->cover }}" alt=" {{ $category->name }} ">
-                                <span> {{ $category->name }} </span>
-                            </li>
-                        @endforeach
-                    </ul>
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <ul class="list-unstyled m-0">
+                            @foreach ($categories as $category)
+                                <li>
+                                    <img src=" {{ $category->cover }}" alt=" {{ $category->name }} ">
+                                    <span> {{ $category->name }} </span>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="highlights">
+        <div class="wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="tile">
+                            <div>
+                                <h2>Any day <br> Offers</h2>
+                                <p class="text-uppercase">new phenomenon <br> burger taste</p>
+                            </div>
+                            <span>$12.90</span>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="tile exp moon">
+                            <div>
+                                <h2>Other <br> flavors</h2>
+                                <p class="text-uppercase">save room. <br> we made salats</p>
+                            </div>
+                            <span>$12.90</span>
+                        </div>
+                    </div>
+
+                    <div class="col-4">
+                        <div class="tile moon location">
+                            <h2>Find a Poco <br> store <br> near you</h2>
+                            <span class="text-uppercase">contact us</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
